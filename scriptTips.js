77c5ff7bcac868593673bf52da327ffdd7bef13d
riyap2601 <140298765+riyap2601@users.noreply.github.com>
2023-10-15 10:37:44 -0700

    Update scriptTips.js

--- scriptTips.js
+++ scriptTips.js
@@ -1,4 +1,4 @@
-var factList = [
+const facts = [
 "The average college student produces 640 pounds of solid waste each year, including 500 disposable cups and 320 pounds of paper.", /*0*/
 "The US makes up only 5% of the world population, but it throws away enough plastic bottles in a week to encircle the Earth 5 times!", /*1*/
 "We toss out two billion plastic razors, a million and a half tons of paper towels, and 12 billion disposable diapers annually.", /*2*/
@@ -6,19 +6,11 @@ var factList = [
 "25% of the bottled water you buy is really municipal tap water, so you might as well pass on the plastic bottle", /*4*/  
 ]
 
-var fact = document.getElementById("fact");
-var factBtn = document.getElementById("factBtn");
-var count = 0;
-
-if (factBtn) {
-  factBtn.addEventListener("click", displayFact);
+function generateRandomFact() {
+    const randomIndex = Math.floor(Math.random() * facts.length);
+    const factElement = document.getElementById("fact");
+    factElement.textContent = facts[randomIndex];
 }
 
-function displayFact () {
-  fact.innerHTML = factList[count];
-  count++;
-  if (count == factList.length) {
-    count = 0;
-  }
-}
+document.getElementById("generateBtn").addEventListener("click", generateRandomFact);
 
